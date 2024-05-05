2810d996a2c2fb2824be9f348c455d99c1274c17
shaker <tanimshaker@gmail.com>
2024-05-05 16:08:28 -0500

    All

---
+++ Course Registration program/Entities/20240504032713_Initial.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Course_Registration_program.Entities
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "course",
+                columns: table => new
+                {
+                    courseID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    courseNumber = table.Column<int>(type: "integer", nullable: false),
+                    courseName = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_course", x => x.courseID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "instructor",
+                columns: table => new
+                {
+                    instructorID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    lName = table.Column<string>(type: "text", nullable: false),
+                    fName = table.Column<string>(type: "text", nullable: false),
+                    emailID = table.Column<string>(type: "text", nullable: false),
+                    courseID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_instructor", x => x.instructorID);
+                    table.ForeignKey(
+                        name: "FK_instructor_course_courseID",
+                        column: x => x.courseID,
+                        principalTable: "course",
+                        principalColumn: "courseID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "student",
+                columns: table => new
+                {
+                    studentID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    lName = table.Column<string>(type: "text", nullable: false),
+                    fName = table.Column<string>(type: "text", nullable: false),
+                    emailID = table.Column<string>(type: "text", nullable: false),
+                    phoneNumber = table.Column<long>(type: "bigint", nullable: false),
+                    courseID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_student", x => x.studentID);
+                    table.ForeignKey(
+                        name: "FK_student_course_courseID",
+                        column: x => x.courseID,
+                        principalTable: "course",
+                        principalColumn: "courseID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_instructor_courseID",
+                table: "instructor",
+                column: "courseID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_student_courseID",
+                table: "student",
+                column: "courseID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "instructor");
+
+            migrationBuilder.DropTable(
+                name: "student");
+
+            migrationBuilder.DropTable(
+                name: "course");
+        }
+    }
+}
---
+++ Course Registration program/Entities/20240505162518_SecondMigration.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Course_Registration_program.Entities
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_student_course_courseID",
+                table: "student");
+
+            migrationBuilder.DropIndex(
+                name: "IX_student_courseID",
+                table: "student");
+
+            migrationBuilder.DropColumn(
+                name: "courseID",
+                table: "student");
+
+            migrationBuilder.CreateTable(
+                name: "studentCourse",
+                columns: table => new
+                {
+                    assignmentID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    studentID = table.Column<int>(type: "integer", nullable: false),
+                    courseID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_studentCourse", x => x.assignmentID);
+                    table.ForeignKey(
+                        name: "FK_studentCourse_course_courseID",
+                        column: x => x.courseID,
+                        principalTable: "course",
+                        principalColumn: "courseID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_studentCourse_student_studentID",
+                        column: x => x.studentID,
+                        principalTable: "student",
+                        principalColumn: "studentID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_studentCourse_courseID",
+                table: "studentCourse",
+                column: "courseID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_studentCourse_studentID",
+                table: "studentCourse",
+                column: "studentID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "studentCourse");
+
+            migrationBuilder.AddColumn<int>(
+                name: "courseID",
+                table: "student",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_student_courseID",
+                table: "student",
+                column: "courseID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_student_course_courseID",
+                table: "student",
+                column: "courseID",
+                principalTable: "course",
+                principalColumn: "courseID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
